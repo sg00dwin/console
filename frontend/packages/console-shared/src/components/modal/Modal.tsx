447a538325fc0a3cfa2bfddc8a9c1909eb981a09
Steve Goodwin <sgoodwin@redhat.com>
2025-01-22 12:58:50 -0500

    Revert console shared modal and operator install modal changes so that it scrolls

--- frontend/packages/console-shared/src/components/modal/Modal.tsx
+++ frontend/packages/console-shared/src/components/modal/Modal.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
-import { ModalProps as PfModalProps } from '@patternfly/react-core';
-import { Modal as PfModal } from '@patternfly/react-core/deprecated';
+// import { ModalProps as PfModalProps } from '@patternfly/react-core';
+import { Modal as PfModal, ModalProps as PfModalProps } from '@patternfly/react-core/deprecated';
 import * as cx from 'classnames';
 import './Modal.scss';
 
