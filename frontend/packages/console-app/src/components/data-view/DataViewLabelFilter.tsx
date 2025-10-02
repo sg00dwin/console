345b8f4fb5b694a69aba0264399d29cd4a5ff145
Steve Goodwin <sgoodwin@redhat.com>
2025-10-02 10:57:50 -0400

    Fix error after upgrading to PF-6.4-prelease packages. Cannot read properties of null (reading current)

--- frontend/packages/console-app/src/components/data-view/DataViewLabelFilter.tsx
+++ frontend/packages/console-app/src/components/data-view/DataViewLabelFilter.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import { ToolbarFilter } from '@patternfly/react-core';
 import * as _ from 'lodash';
 import { useTranslation } from 'react-i18next';
 import { useSearchParams } from 'react-router-dom-v5-compat';
@@ -16,7 +15,6 @@ type DataViewLabelFilterProps<TData> = {
 
 export const DataViewLabelFilter = <TData extends K8sResourceCommon = K8sResourceCommon>({
   data,
-  title,
   filterId,
   onChange,
   showToolbarItem,
@@ -25,40 +23,34 @@ export const DataViewLabelFilter = <TData extends K8sResourceCommon = K8sResourc
 
   const [searchParams] = useSearchParams();
   const [labelInputText, setLabelInputText] = React.useState('');
-  const labelSelection = searchParams.get(filterId)?.split(',') ?? [];
+  const labelSelection =
+    searchParams
+      .get(filterId)
+      ?.split(',')
+      .filter((label) => label) ?? [];
 
   const applyLabelFilters = (values: string[]) => {
     setLabelInputText('');
     onChange?.(filterId, values.join(','));
   };
 
+  if (!showToolbarItem) {
+    return null;
+  }
+
   return (
-    <ToolbarFilter
-      categoryName={title}
-      labels={labelSelection}
-      showToolbarItem={showToolbarItem}
-      deleteLabel={(_category, label: string) => {
-        setLabelInputText('');
-        applyLabelFilters(_.difference(labelSelection, [label]));
-      }}
-      deleteLabelGroup={() => {
-        setLabelInputText('');
-        applyLabelFilters([]);
-      }}
-    >
-      <div className="pf-v6-c-input-group co-filter-group">
-        <AutocompleteInput
-          color="purple"
-          onSuggestionSelect={(selected) => {
-            applyLabelFilters(_.uniq([...labelSelection, selected]));
-          }}
-          showSuggestions
-          textValue={labelInputText}
-          setTextValue={setLabelInputText}
-          placeholder={t('public~Filter by label')}
-          data={data}
-        />
-      </div>
-    </ToolbarFilter>
+    <div className="pf-v6-c-input-group co-filter-group">
+      <AutocompleteInput
+        color="purple"
+        onSuggestionSelect={(selected) => {
+          applyLabelFilters(_.uniq([...labelSelection, selected]));
+        }}
+        showSuggestions
+        textValue={labelInputText}
+        setTextValue={setLabelInputText}
+        placeholder={t('public~Filter by label')}
+        data={data}
+      />
+    </div>
   );
 };
